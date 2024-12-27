dc1a52ed68b474675689cd42d8bebfbfdd7c2ef4
shivaprasad918 <pshivaprasad783@gmail.com>
2024-12-27 16:06:37 +0530

    add routes and pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Profile from "./Pages/Profile";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <div className="text-center text-blue-500">Hello Tailwind</div>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div><h1>Hello  machaaa</h1></div>
+    </div>
+  )
+}
+
+export default Home
