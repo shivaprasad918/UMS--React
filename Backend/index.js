fde36696241ff288b2bd9e76311777162cc0b7a7
shivaprasad918 <pshivaprasad783@gmail.com>
2024-12-28 13:56:58 +0530

    conect to db

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to Mongodb'); 
+}).catch((error) => {
+    console.log(error);
+})
+
 
 const app = express();
 
