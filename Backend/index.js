045fef8c95b84da3d4345ddd74ce746747c816af
shivaprasad918 <pshivaprasad783@gmail.com>
2024-12-29 22:41:18 +0530

    complete sign up page

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import userRouter from './Routes/userRoute.js';
-import authRoute from './Routes/authRoute.js';
-import dotenv from 'dotenv';
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./Routes/userRoute.js";
+import authRoute from "./Routes/authRoute.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to Mongodb'); 
-}).catch((error) => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Mongodb");
+  })
+  .catch((error) => {
     console.log(error);
-})
-
+  });
 
 const app = express();
 
 app.use(express.json());
 
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
-    
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRoute);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.statusCode || 'Internel Server Eror';
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    })
 })
-
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRoute)
